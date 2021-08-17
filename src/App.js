83c06b6232119fcc16ae4be4b1b2906224648fd3
Benjamin Alloway <benalloway@gmail.com>
2021-08-17 12:33:31 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,81 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import JobApplicationForm from "./components/JobApplicationForm";
-import postJobApplication from "./async/snag-application-api"
+import {postJobApplication, getJobApplications} from "./async/snag-application-api"
 
 function App() {
 
-  // data
-  // { Id: "1", Question: "do you own a car?", Answer: "yes" }, 
-  // { Id: "2", Question: "do you have a valid license?", Answer: "yes" }, 
-  // { Id: "3", Question: "have you ever had a DUI?", Answer: "no" },
-  // { Id: "4", Question: "are you willing to drive more than 1000 miles a month?", Answer: "yes" },
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/applicationForm">Application Form</Link>
+          </li>
+          <li>
+            <Link to="/applicationsList">Applications List</Link>
+          </li>
+        </ul>
 
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/applicationForm">
+            <ApplicationForm />
+          </Route>
+          <Route path="/applicationsList">
+            <ApplicationsList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function ApplicationForm() {
   function handleSubmit(formData) {
     postJobApplication(JSON.stringify(formData))
     console.log(formData)
   }
+  return (
+      <JobApplicationForm onSubmit={handleSubmit} />
+  );
+}
+
+function ApplicationsList() {
+  const applications = getJobApplications().then((data, error) => console.log(data))
 
   return (
-    <JobApplicationForm onSubmit={handleSubmit} />
+    <div>
+      <h2>Accepted Applications: </h2>
+    </div>
   );
 }
 
--- src/async/snag-application-api.js
+++ src/async/snag-application-api.js
@@ -1,19 +1,21 @@
-const API_URL = process.env.API_URL ?? ""
+const API_URL = process.env.API_URL ?? "http://0.0.0.0:8000"
 
-export default async function postJobApplication({Name, Questions}) {
+console.log("api url: ", API_URL)
+
+export async function postJobApplication({Name, Questions}) {
     let data, error;
 
     try {
-    const response = await fetch(`${API_URL}/applications`, {
-        method: "POST",
-        body: JSON.stringify({
-          Name,
-          Questions,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-    });
+      const response = await fetch(`${API_URL}/applications`, {
+          method: "POST",
+          body: JSON.stringify({
+            Name,
+            Questions,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+      });
 
     data = await response.json();
 
@@ -27,4 +29,29 @@ export default async function postJobApplication({Name, Questions}) {
   }
 
   return { data, error };
+}
+
+export async function getJobApplications() {
+  let data, error;
+
+  try {
+    const response = await fetch(`${API_URL}/applications`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+    });
+
+  data = await response.json();
+
+  if (!response.ok) {
+    error = data.error;
+    data = null;
+  }
+} catch (err) {
+  error = err;
+  data = null;
+}
+
+return { data, error };
 }
