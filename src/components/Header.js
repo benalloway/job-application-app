2f79fcd8a5561994f401e47c6aba62151250ca49
Benjamin Alloway <benalloway@gmail.com>
2021-08-18 15:03:27 -0400

    small ui fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
     return (
       <header className="bg-indigo-600">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-          <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+          <div className="w-full py-6 flex items-center justify-center border-b border-indigo-500 lg:border-none">
             <div className="flex items-center">
               <Link to="/">
                 <span className="sr-only">Applications API</span>
@@ -24,7 +24,7 @@ import { Link } from "react-router-dom";
               </div>
             </div>
           </div>
-          <div className="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+          <div className="py-4 flex flex-wrap justify-center space-x-4 lg:hidden">
             {navigationElements.map((link) => (
               <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
                 {link.name}
